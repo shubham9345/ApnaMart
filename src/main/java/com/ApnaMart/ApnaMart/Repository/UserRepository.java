88581c03631af02f45ee941b097e008b405927c9
shubham <kumarshubham2005a@gmail.com>
2024-11-01 12:05:42 +0530

    feature updated

--- src/main/java/com/ApnaMart/ApnaMart/Repository/UserRepository.java
+++ src/main/java/com/ApnaMart/ApnaMart/Repository/UserRepository.java
@@ -1,11 +1,7 @@
 package com.ApnaMart.ApnaMart.Repository;
 
-import com.ApnaMart.ApnaMart.Model.Order;
 import com.ApnaMart.ApnaMart.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
